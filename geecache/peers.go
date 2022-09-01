2be264778ab8fca93c66b989201917a0ab584b5c
jcbjcbjc <91889375+jcbjcbjc@users.noreply.github.com>
2022-09-01 22:58:59 +0800

    update

---
+++ geecache/peers.go
@@ -0,0 +1,14 @@
+package geecache
+
+import pb "github.com/jcbjcbjc/Gee/geecache/geecachepb"
+
+// PeerPicker is the interface that must be implemented to locate
+// the peer that owns a specific key.
+type PeerPicker interface {
+	PickPeer(key string) (peer PeerGetter, ok bool)
+}
+
+// PeerGetter is the interface that must be implemented by a peer.
+type PeerGetter interface {
+	Get(in *pb.Request, out *pb.Response) error
+}
--- go.mod
+++ go.mod
@@ -2,3 +2,4 @@ module github.com/jcbjcbjc/Gee
 
 go 1.17
 
+require github.com/golang/protobuf v1.3.3
